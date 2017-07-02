3ba114b8560ee86012f5b9e34282f78e15b8ed8d
Hotanya <hotanya.r@gmail.com>
2017-07-02 22:13:58 +1200

    removed some hard coding, added functions and comments to make code clearer

--- game.js
+++ game.js
@@ -2,7 +2,8 @@ var canvas = "";
 var canvasContext = "";
 var paddleY = 50;
 var ballX = 50;
-var ballSpeed = 5;
+var ballSpeed = 10;
+var paddleSpeed = 20;
 
 window.onload = function(){
 var fps = 30;
@@ -13,42 +14,46 @@ setInterval(move, 1000/fps);
 
 draw();
 move();
-
 }
-
-
-function move(){
-ballX += ballSpeed;
-    
-    // Moves paddles 20px 
-    paddleY += 20;
-    console.log(paddleY);
-    if(ballX >= 800){
-        ballSpeed = -5;
-    }
-    else if(ballX <= 0){
-        ballSpeed = 5;
-    }
-}
-
 function draw(){
-
-    // sets colour and size of canvas
+    // used to reference the gamCanvas element in the html code
     canvas = document.getElementById("gameCanvas");
     canvasContext =canvas.getContext("2d");
-    canvasContext.fillStyle = "black";
-    canvasContext.fillRect(0,0,canvas.width,canvas.height);
+    
+    // sets colour and size of canvas
+    drawRect(0,0,canvas.width,canvas.height, "black");
 
-    // creates a block of specified size
-    canvasContext.fillStyle = "white";
-    canvasContext.fillRect(1,paddleY,10,120);
+    // creates the paddle of specified size and colour
+    drawRect(1,paddleY,10,120,"white");
 
-    canvasContext.fillStyle = "red";
-    canvasContext.fillRect(ballX,100,10,10);
-        
-
+    // creates the ball of specified size and colour
+    drawRect(ballX,100,10,10,"red");
 }
 
-function reverse(){
-    ballX = ballX - 5;
+function drawRect(leftX,topY, width,height, colour){
+    canvasContext.fillStyle = colour;
+    canvasContext.fillRect(leftX,topY, width,height);
+    
+}
+
+// function which handles all the movement stuff
+function move(){
+
+// bounces ball off the sides
+ballX += ballSpeed;
+    if(ballX >= canvas.width){
+        ballSpeed = -ballSpeed;
+    }
+    else if(ballX <= 0){
+        ballSpeed = -ballSpeed;
+    }
+
+// bounces paddle off the sides     
+paddleY += paddleSpeed;
+if(paddleY >= (canvas.height)-120){ //minusing the height of the paddle
+        paddleSpeed = -paddleSpeed;
+    }
+    else if(paddleY <= 0){
+        paddleSpeed = -paddleSpeed;
+    }
 }
