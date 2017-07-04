874a0eeba2df1f129405e457e149d7b202640b93
Hotanya <hotanya.r@gmail.com>
2017-07-04 18:32:50 +1200

    changed shape of ball to a sphere, added vertical ball movement, added code to make paddle follow mouse- not working for some reason

--- game.js
+++ game.js
@@ -1,9 +1,25 @@
 var canvas = "";
 var canvasContext = "";
-var paddleY = 50;
 var ballX = 50;
-var ballSpeed = 10;
+var ballY = 50;
+var ballSpeedX = 10;
+var ballSpeedY = 4;
+var paddle1Y = 250;
+const paddleHeight = 100;
 var paddleSpeed = 20;
+
+function mousePosition(event){
+    var canvasRect = canvas.getBoundingClientRect();
+    var outside = document.documentElement;
+
+    var mouseX = event.clientX - canvasRect.left - outside.scrollLeft;
+    var mouseY = event.clientY - canvasRect.top - outside.scrollTop;
+    
+    return {
+        x:mouseX,
+        y:mouseY
+    };
+}
 
 window.onload = function(){
 var fps = 30;
@@ -14,6 +30,12 @@ setInterval(move, 1000/fps);
 
 draw();
 move();
+
+canvas.addEventListener("mouseMovement",
+    function(event){
+        var mousePos = mousePosition(event);
+        paddle1Y = mousePos.y;
+    });
 }
 function draw(){
     // used to reference the gamCanvas element in the html code
@@ -24,12 +46,20 @@ function draw(){
     drawRect(0,0,canvas.width,canvas.height, "black");
 
     // creates the paddle of specified size and colour
-    drawRect(1,paddleY,10,120,"white");
+    drawRect(1,paddle1Y ,10,120,"white");
 
     // creates the ball of specified size and colour
-    drawRect(ballX,100,10,10,"red");
+    drawBall(ballX,ballY,10,0,Math.PI*2,"white",true);// pi*2 makes full circle
 }
 
+function drawBall(centerX,centerY,radius,startAngle,endAngle,colour,bool){
+    canvasContext.beginPath(); //needs to be before .arc
+    canvasContext.arc(centerX,centerY,radius,startAngle,endAngle,colour,bool);
+    canvasContext.fillStyle = colour;
+    canvasContext.fill();
+
+
+}
 function drawRect(leftX,topY, width,height, colour){
     canvasContext.fillStyle = colour;
     canvasContext.fillRect(leftX,topY, width,height);
@@ -38,22 +68,29 @@ function drawRect(leftX,topY, width,height, colour){
 
 // function which handles all the movement stuff
 function move(){
-
 // bounces ball off the sides
-ballX += ballSpeed;
+ballX += ballSpeedX;
     if(ballX >= canvas.width){
-        ballSpeed = -ballSpeed;
+        ballSpeedX = -ballSpeedX;
     }
     else if(ballX <= 0){
-        ballSpeed = -ballSpeed;
+        ballSpeedX = -ballSpeedX;
     }
 
+ballY += ballSpeedY;
+    if(ballY >= canvas.height){
+        ballSpeedY = -ballSpeedY;
+    }
+    else if(ballY <= 0){
+        ballSpeedY = -ballSpeedY;
+    }    
+
 // bounces paddle off the sides     
-paddleY += paddleSpeed;
-if(paddleY >= (canvas.height)-120){ //minusing the height of the paddle
-        paddleSpeed = -paddleSpeed;
-    }
-    else if(paddleY <= 0){
-        paddleSpeed = -paddleSpeed;
-    }
+// paddle1Y += paddleSpeed;
+// if(paddle1Y >= (canvas.height)-120){ //minusing the height of the paddle
+//         paddleSpeed = -paddleSpeed;
+//     }
+//     else if(paddleY <= 0){
+//         paddleSpeed = -paddleSpeed;
+//     }
 }
