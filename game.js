381cc13cbbe316d952ebb8428b282bcecfec1c16
Hotanya <hotanya.r@gmail.com>
2017-07-22 18:09:56 +1200

    right paddle now follows ball movemnt

--- game.js
+++ game.js
@@ -47,16 +47,60 @@ function reset(){
     ballY = canvas.height/2;
 }
 
+function paddle2Movement(){
+    var paddle2YCenter = paddle2Y + (PADDLE_HEIGHT/2);
+    if(paddle2YCenter < ballY){
+        paddle2Y += 6;
+    }
+    else{
+        paddle2Y -= 6;
+    }
+}
+
+// function which handles all the movement stuff
+function move(){ // bounces ball off the sides
+    
+paddle2Movement(); 
+
+ballX += ballSpeedX;
+    
+    if(ballX <= 0){
+        if(ballY > paddle1Y && ballY < paddle1Y + PADDLE_HEIGHT){ 
+        ballSpeedX = -ballSpeedX; //deflects the ball off the paddle           
+        }
+        else{
+            reset();
+        }
+    }
+
+    else if(ballX >= canvas.width){
+        if(ballY > paddle2Y && ballY < paddle2Y + PADDLE_HEIGHT){ 
+        ballSpeedX = -ballSpeedX;//deflects the ball off the paddle            
+        }
+        else{
+            reset();
+        }
+    }
+ballY += ballSpeedY;
+    if(ballY >= canvas.height){
+        ballSpeedY = -ballSpeedY;
+    }
+    else if(ballY <= 0){
+        ballSpeedY = -ballSpeedY;
+    }    
+
+}
+
 function draw(){
     
     // sets colour and size of canvas
     drawRect(0,0,canvas.width,canvas.height, "black");
 
     // creates the paddle of specified size and colour
-    drawRect(1,paddle1Y ,PADDLE_THICKNESS,120,"white");
+    drawRect(1,paddle1Y ,PADDLE_THICKNESS,PADDLE_HEIGHT,"white");
 
     // creates the paddle of specified size and colour
-    drawRect(canvas.width-11,paddle2Y ,PADDLE_THICKNESS,120,"white");
+    drawRect(canvas.width-11,paddle2Y ,PADDLE_THICKNESS,PADDLE_HEIGHT,"white");
 
     // creates the ball of specified size and colour
     drawBall(ballX,ballY,10,0,Math.PI*2,"white",true);// pi*2 makes full circle
@@ -74,30 +118,4 @@ function drawRect(leftX,topY, width,height, colour){
     canvasContext.fillStyle = colour;
     canvasContext.fillRect(leftX,topY, width,height);
     
-}
-
-// function which handles all the movement stuff
-function move(){
-// bounces ball off the sides
-ballX += ballSpeedX;
-    if(ballX >= canvas.width){
-        ballSpeedX = -ballSpeedX;
-    }
-    else if(ballX <= 0){
-        if(ballY > paddle1Y && ballY < paddle1Y + PADDLE_HEIGHT){ //deflects the ball off the paddle
-        ballSpeedX = -ballSpeedX;            
-        }
-        else{
-            reset();
-        }
-    }
-
-ballY += ballSpeedY;
-    if(ballY >= canvas.height){
-        ballSpeedY = -ballSpeedY;
-    }
-    else if(ballY <= 0){
-        ballSpeedY = -ballSpeedY;
-    }    
-
 }
